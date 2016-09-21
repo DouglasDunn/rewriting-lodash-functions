b31d3d9fa28282e586fdcfe755a8cca63541ae3f
Phillip Lorenzo <pllearns@gmail.com>
2016-09-21 12:09:52 -0700

    Created and tested flowRight function (#5)

--- spec/flow_right_spec.js
+++ spec/flow_right_spec.js
@@ -1,8 +1,28 @@
-// import{flowRight} from '../dist/flow_right'
-//
-// describe('flowRight'), () =>{
-//   it('Executes the functions in the function from right to left', () => {
-//     expect()
-//   })
-//
-// }
+import { flowRight } from '../dist/flow_right'
+
+const add = (a, b) => a + b
+const square = a => a * a
+
+fdescribe('flowRight', () =>{
+  it( 'returns a function', () => {
+    expect( typeof( flowRight([ square, add ]) )).toEqual( 'function' )
+  })
+
+  it( 'raises a TypeError when functions are not supplied', () => {
+    expect( () => flowRight( 1 ) ).toThrowError( TypeError )
+  })
+
+  it( 'operates on a single function', () => {
+    const testFunction = flowRight( square )
+
+    expect( testFunction( 2 ) ).toEqual( 4 )
+  })
+
+  it( 'executes functions in array from right to left', () => {
+    expect( flowRight([ square, add ])( 3, 4 ) ).toEqual( 49 )
+  })
+
+  it( 'executes functions as params from right to left', () => {
+    expect( flowRight( square, add )( 3, 4 ) ).toEqual( 49 )
+  })
+})
--- src/flow_right.js
+++ src/flow_right.js
@@ -1,8 +1,23 @@
-// const flowRight = ( collection ) => {
-//   for ( let i=collection.length - 1; i >= 0; i-- ){
-//
-//   }
-//
-// }
-//
-// export { flowRight }
+const functionReducer = (memo, fn) =>
+  memo && typeof( fn ) === 'function'
+
+const allFunctions = functions =>
+  functions.reduce( functionReducer, true )
+
+const ensureArray = (...functions) =>
+  functions.reduce( (memo, item ) => memo.concat( item ), [] )
+
+const flowRight = (...functions) => {
+  const fns = ensureArray( ...functions ).reverse()
+
+  if( ! allFunctions( fns ) ) {
+    throw new TypeError( 'Expected a Function' )
+  }
+
+  return (...args) =>
+    fns.slice( 1 ).reduce(
+      (memo, next) => next( memo ), fns[ 0 ]( ...args )
+    )
+}
+
+export { flowRight }
