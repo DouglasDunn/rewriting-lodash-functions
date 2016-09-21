0cca23eb7b63fd0d0b4a051c2ce6884c248c98c3
Diana Vashti <Diana.vashti@gmail.com>
2016-09-21 11:01:58 -0700

    completed and tested subtract (#4)

---
+++ spec/subtract_spec.js
@@ -0,0 +1,10 @@
+import { subtract } from '../dist/subtract'
+
+  describe( 'subtract', () => {
+    it('subtracts two numbers', () => {
+      expect( subtract(3,2).toEqual(1))
+    })
+
+    it('returns NaN if input is null or undefined')
+      expect( subtract().toEqual(NaN))
+  })
