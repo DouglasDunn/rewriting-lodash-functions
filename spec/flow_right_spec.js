695d06cdca60f7562100bacba536c2c385ef9f3f
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-09-21 09:15:23 -0700

    trying to finish this difficult function, will defer to a pair programming observation

---
+++ spec/flow_right_spec.js
@@ -0,0 +1,8 @@
+// import{flowRight} from '../dist/flow_right'
+//
+// describe('flowRight'), () =>{
+//   it('Executes the functions in the function from right to left', () => {
+//     expect()
+//   })
+//
+// }
