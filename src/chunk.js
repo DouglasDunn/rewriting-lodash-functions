752a539134a83e011d67f9292b208945c0ff71b1
Phillip Lorenzo <pllearns@gmail.com>
2016-09-21 17:17:56 -0700

    Created and tested chunk

--- src/chunk.js
+++ src/chunk.js
@@ -1,22 +1,19 @@
 const chunk = (collection, chunkSize=1) => {
-  if ( ! (collection instanceof Array) ){
-    return []
-  }
-
-  let result = []
-
-  for( let index = 0; index < collection.length; index += chunkSize ) {
-    let babyResult = []
-
-    for( let baby = 0; baby < chunkSize && index + baby < collection.length; baby ++) {
-      babyResult.push( collection[ index + baby])
+    if ( ! (collection instanceof Array) ){
+      return []
     }
 
-    result.push( babyResult )
-  }
+    let result = []
 
-  return result
+    for( let index = 0; index < collection.length; index += chunkSize ) {
+      let babyResult = []
+
+      for( let baby = 0; baby < chunkSize && index + baby < collection.length; baby ++) {
+        babyResult.push( collection[ index + baby] )
+      }
+      result.push(babyResult)
+    }
+    return result
 }
-
 
 export { chunk }
