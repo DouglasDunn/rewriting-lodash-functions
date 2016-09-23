67f9a5aea9c2c21f0a49156714df7bf897052a20
Phillip Lorenzo <pllearns@gmail.com>
2016-09-23 10:56:10 -0700

    Finished times function and started documentation for our functions

--- spec/times_spec.js
+++ spec/times_spec.js
@@ -2,10 +2,6 @@ import { times } from '../dist/times'
 
 describe( 'times', () => {
   it( 'invokes a function a set amount of times and returns an array of results', () => {
-    expect( times(3, toString()).toEqual( ['0', '1', '2'] ))
-  })
-
-  it( 'returns an empty array if it is not fed a function', () => {
-    expect( times( 1 )).toEqual( [ ] )
+    expect( times(3, String)).toEqual( ['0', '1', '2'] )
   })
 })
--- src/times.js
+++ src/times.js
@@ -1,13 +1,12 @@
 const times = (n, func) => {
-  if ( ! typeof func) {
-    return []
-    }
+  
   let result = []
     for(let i = 0; i < n; i++){
-      result.push(i)
-      }
-    return result
-}
+      let val = func(i)
+      result.push( val )
+    }
 
+    return result
+  }
 
 export { times }
